5722ca8703e65a2994d918a0b07af8abec30f34b
Greg Wagner <gpwagner@gmail.com>
2020-01-11 03:55:13 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push (array, element)
+  array.push(element)
+end
+
+def using_unshift (array, element)
+  array.unshift(element)
+end
+
+def using_pop (array)
+  array.pop
+end
+
+def pop_with_args (array, number_to_pop)
+  array.pop(number_to_pop)
+end
+
+def using_shift (array)
+  array.shift
+end
+
+def shift_with_args (array, number_to_shift)
+  array.shift(number_to_shift)
+end
+
+def using_concat (array1, array2)
+  array1.concat(array2)
+end
