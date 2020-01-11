1fe93891e54074fc599eae75b884006557d36393
Greg Wagner <gpwagner@gmail.com>
2020-01-11 04:03:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,19 @@ end
 def using_insert (array, element)
   array.insert(3, element)
 end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete (array, element)
+  array.delete(element)
+end
+
+def using_delete_at (array, index)
+  array.delete_at(index)
+end
